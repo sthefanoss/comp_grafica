17518f835d3e1d3af6c820885a6fefe31d81fb9d
sthefano <sthefanoss@gmail.com>
2022-08-25 23:37:39 -0300

    Initial Commit

---
+++ computacao_grafica/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  computacao_grafica
+//
+//  Created by Sthefano Schiavon on 25/08/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
